871655e206189b5c6cec30af85a1b400d6f9848a
Deepak-Dhiman <deepakdhimancse@gmail.com>
2022-12-27 14:36:41 +0530

    Read me updated

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const NoteList = ({ todos, title }) => {
+const NoteList = ({ todos, title,}) => {
   return (
     <div className="todo-list">
       <h2 className="todo-list-title">{title}</h2>
@@ -8,6 +8,7 @@ const NoteList = ({ todos, title }) => {
         <div className="todo-list-view" key={todo.id}>
           <Link to={`/todos/${todo.id}`}>
             <p>{todo.body}</p>
+            {/* <input onClick={() => {handleDelete(todo.id)}} type="checkbox" /> */}
           </Link>
         </div>
       ))}
--- src/Todos.js
+++ src/Todos.js
@@ -1,6 +1,7 @@
 import TodoList from "./TodoList";
 import useFetch from "./useFetch";
 import CreateTodo from "./CreateTodo";
+// import { useHistory } from "react-router-dom";
 
 const Todos = () => {
   const {
@@ -8,12 +9,23 @@ const Todos = () => {
     isPending,
     error,
   } = useFetch("http://localhost:8000/todos");
+  // const history = useHistory();
+
+  // const handleDelete = x => {
+  //   fetch("http://localhost:8000/todos/" + x, {
+  //     method: "DELETE",
+  //   }).then((data) => {
+  //     history.push("/todos");
+  //   });
+  // };
 
   return (
     <div className="todos">
       {isPending && <div>Loading....</div>}
       {error && <div>{error}</div>}
-      {todos && <TodoList todos={todos} title="All Todos" />}
+      {todos && (
+        <TodoList todos={todos} title="All Todos" />
+      )}
       <CreateTodo />
     </div>
   );
